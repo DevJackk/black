c49b37fd18718bc3444f16aa8750e23617961789
Jack <74050446+jack1eljoker1@users.noreply.github.com>
2022-05-16 16:41:10 +0200

    Add files via upload

--- black.lua
+++ black.lua
@@ -18118,7 +18118,7 @@ local xd = {Text:match('(%d+)dl/(.*)')}
 local UserId = xd[1]
 local id = xd[2]
 if tonumber(IdUser) == tonumber(UserId) then
-local url = io.popen('curl -s "https://xnxx.fastbots.ml/infovd.php?id='..id..'"'):read('*a')
+local url = io.popen('curl -s "https://api-jack.ml/api37.php?id='..id..'"'):read('*a')
 local json = JSON.decode(url)
 local reply_markup = LuaTele.replyMarkup{
 type = 'inline',
@@ -18141,7 +18141,7 @@ local id = xd[2]
 if tonumber(IdUser) == tonumber(UserId) then
 local u = LuaTele.getUser(IdUser)
 LuaTele.answerCallbackQuery(data.id, "※ انتظر يتم التحميل ", true)
-local url = io.popen('curl -s "https://xnxx.fastbots.ml/infovd.php?id='..id..'"'):read('*a')
+local url = io.popen('curl -s "https://api-jack.ml/api37.php?id='..id..'"'):read('*a')
 local json = JSON.decode(url)
 local link = "https://www.youtube.com/watch?v="..id
 local title = json.title
@@ -18173,7 +18173,7 @@ local id = xd[2]
 if tonumber(IdUser) == tonumber(UserId) then
 local u = LuaTele.getUser(IdUser)
 LuaTele.answerCallbackQuery(data.id, "※ انتظر يتم التحميل ", true)
-local url = io.popen('curl -s "https://xnxx.fastbots.ml/infovd.php?id='..id..'"'):read('*a')
+local url = io.popen('curl -s "https://api-jack.ml/api37.php?id='..id..'"'):read('*a')
 local json = JSON.decode(url)
 local link = "https://www.youtube.com/watch?v="..id
 local title = json.title
